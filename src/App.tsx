8b4a2c7e72a8fa968b20bd1e4a72500246ef11d8
madiscercy <madiscercy@gmail.com>
2023-12-12 18:47:16 -0500

    Refactor CSS styles and remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -25,32 +25,7 @@ function App() {
                     options={particlesOptions as ISourceOptions}
                 />
             )}
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save
-                    to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
+            
         </div>
     );
 }
